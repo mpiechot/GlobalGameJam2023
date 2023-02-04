5b07c7d5e0ed1aab349a87cd4d543d94d1763bfd
mpiechot <marco.piechotta@gmail.com>
2023-02-04 23:10:24 +0100

    Update LevelUp to MultiplayerCode

--- CompetitiveTreePlanting/Assets/Tree/scripts/LevelUp.cs
+++ CompetitiveTreePlanting/Assets/Tree/scripts/LevelUp.cs
@@ -1,9 +1,10 @@
+using Fusion;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
-public class LevelUp : MonoBehaviour
+public class LevelUp : NetworkBehaviour
 {
     public UnityEvent TreeGrownUp;
 
@@ -16,7 +17,7 @@ public class LevelUp : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        PlaceTree();
+        RPC_PlaceTree(level);
     }
 
     public void OnLevelUp()
@@ -29,13 +30,18 @@ public class LevelUp : MonoBehaviour
         {
             TreeGrownUp?.Invoke();
         }
-        PlaceTree();
+
+        if (Object.HasInputAuthority)
+        {
+            RPC_PlaceTree(level);
+        }
     }
 
-    private void PlaceTree()
+    [Rpc(RpcSources.InputAuthority, RpcTargets.All)]
+    private void RPC_PlaceTree(int received_level, RpcInfo info = default)
     {
         if(currentTree != null) Destroy(currentTree);
-        GameObject newTree = Instantiate(treeLvls[level]);
+        GameObject newTree = Instantiate(treeLvls[received_level]);
         newTree.transform.parent = transform;
         newTree.transform.localPosition = Vector3.zero;
         currentTree = newTree;
