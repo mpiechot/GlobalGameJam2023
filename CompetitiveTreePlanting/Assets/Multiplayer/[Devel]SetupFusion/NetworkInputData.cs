b5ea929f89ca511aeafaacc4944eebcb03b3d1be
Tom Project Seed <THaesch@users.noreply.github.com>
2023-02-05 15:28:16 +0100

    Update NetworkInputData.cs

--- CompetitiveTreePlanting/Assets/Multiplayer/[Devel]SetupFusion/NetworkInputData.cs
+++ CompetitiveTreePlanting/Assets/Multiplayer/[Devel]SetupFusion/NetworkInputData.cs
@@ -7,7 +7,7 @@ namespace MultiplayerDev
     {
         public const byte INTERACT = 0x01;
         public const byte HIT = 0x02;
-        public const byte DASH = 0x03;
+        public const byte DASH = 0x04;
 
         public byte buttons;
         public Vector3 direction;
